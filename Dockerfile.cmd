0c90a1135b17dedd36baca7b95b1429f14ab3852
rewreu <rewreu@Gmail.com>
2018-08-14 15:30:19 -0400

    Update Dockerfile.cmd

--- Dockerfile.cmd
+++ Dockerfile.cmd
@@ -1,6 +1,6 @@
 FROM kinetica/kinetica-intel:latest
 
-LABEL maintainer="Zhe Wu <zwu@kinetica.com>"
+LABEL maintainer="Zhe Wu  <zwu@kinetica.com>"
 
 CMD usermod -a -G fuse gpudb_proc
 CMD chgrp fuse /dev/fuse
